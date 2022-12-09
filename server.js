c0cb090b04d9b29919f7d90927ff6ccd97c2c1ad
Harshit Mishra <harshitmishra2011@gmail.com>
2022-12-09 20:58:57 +0530

    added database models

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
+//packages
+
 require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+
+//connection port and host
+
 port=process.env.PORT || 3000;
 host=process.env.HOST || "localhost";
 
-mongoose.connect(`${process.env.mongodb}/gallery`);
+
+//mongodb connection and initialization
+
+mongoose.connect(`${process.env.mongodb}/assessment`);
+
+const galleryCategorySchema = mongoose.Schema({
+    name: String,
+    createdAt: Date,
+    updatedAt: Date
+})
+
+const imagesGallerySchema = mongoose.Schema({
+    name: String,
+    createdAt: Date,
+    updatedAt: Date,
+    category: Array,
+    likes: Number,
+    imageLink: String
+})
+
+const galleryCategory = mongoose.model("GalleryCategory", galleryCategorySchema);
+const imagesGallery = mongoose.model("ImagesGallery", imagesGallerySchema);
+
+
+//api callings
 
 app.get("/api/health", function(req, res){
     res.send(`Backend server is status: active at time: ${new Date()}`);
 })
+
+
+//creating server
 
 app.listen(port, function(err){
     if(err){

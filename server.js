9a5981209051cd884214f08e1185d598cf976fbd
Harshit Mishra <harshitmishra2011@gmail.com>
2022-12-09 19:07:31 +0530

    added database config and initialization

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 port=process.env.PORT || 3000;
 host=process.env.HOST || "localhost";
 
+mongoose.connect(`${process.env.mongodb}/gallery`);
 
-app.get("/", function(req, res){
-    res.send("hello");
+app.get("/api/health", function(req, res){
+    res.send(`Backend server is status: active at time: ${new Date()}`);
 })
 
 app.listen(port, function(err){
@@ -15,6 +17,6 @@ app.listen(port, function(err){
         console.log(err);
     }
     else{
-        console.log("listening on http://" + host + ":" + port);
+        console.log(`listening on http://${host}:${port}`);
     }
 })
